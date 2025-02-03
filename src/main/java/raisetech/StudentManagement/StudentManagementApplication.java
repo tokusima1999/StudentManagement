1924507b37dd6e9abae32f5e016f92488d36d9c4
taoka nobuhide <l1807334905as@gmail.com>
2025-02-03 11:48:41 +0900

    課題の提出です。

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,13 +1,25 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+  @Autowired
+  private StudentRepository repository;
 
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
+
+  @GetMapping("/StudentsCoursesList")
+  public List<StudentsCourses> getStudentsCoursesList() {
+    return repository.search();
+  }
 }
