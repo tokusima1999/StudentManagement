69cbd5011d56880afc843cbb8079503ab052021f
taoka nobuhide <l1807334905as@gmail.com>
2025-02-18 11:22:21 +0900

    課題15を提出します。受講生情報の登録処理を行いました。

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,16 +1,24 @@
 package raisetech.StudentManagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
 @Mapper
 public interface StudentRepository {
+
   @Select("SELECT * FROM students")
   List<Student> search();
 
   @Select("SELECT * FROM students_courses")
   List<StudentsCourses> searchStudentsCourses();
+
+  @Insert("INSERT INTO students(name,hurigana,nickname,address,area,years,gender,remark,isDeleted) "
+      + "VALUES(#{name}, #{hurigana}, #{nickname}, #{address}, #{area}, #{years}, #{gender}, #{remark},false)")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudent(Student student);
 }
