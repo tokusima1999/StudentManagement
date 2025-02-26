5f8a7b025f262242dbd55401b0a561ed6e19dcd3
taoka nobuhide <l1807334905as@gmail.com>
2025-02-26 11:52:59 +0900

    課題17を提出します。更新作業の実装、キャンセルしたデータが反映されない形でのキャンセルボタンの設置を行いました。

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
