35b4f930816bfcceb1720ed30f8c55d40b8a0791
taoka nobuhide <l1807334905as@gmail.com>
2025-03-04 14:18:41 +0900

    登録処理の実装、リファクタリングを行いました。。

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -9,44 +9,89 @@ import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
+/**
+ * 受講生テーブル情報と受講生コース情報テーブルに紐づくRepositoryです。
+ */
 @Mapper
 public interface StudentRepository {
 
+  /**
+   * 受講生の全件検索を実行します。
+   * @return　全件の受講生一覧
+   */
   @Select("SELECT * FROM students")
   List<Student> search();
 
-
+  /**
+   * 受講生検索を実行します。
+   * @param id 　受講生ID
+   * @return　受講生情報
+   */
   @Select("SELECT * FROM students WHERE id=#{id}")
   Student searchStudent(Long id);
 
+  /**
+   * 受講生のコース情報の全件検索を実行します。
+   * @return　全件の受講生のコース情報
+   */
   @Select("SELECT * FROM students_courses")
   List<StudentsCourses> searchStudentsCoursesList();
 
+  /**
+   * 受講生IDに紐づく受講生のコース情報の検索を実行します。
+   * @param studentID 　受講生ID
+   * @return　受講生IDに紐づく受講生のコース情報
+   */
   @Select("SELECT * FROM students_courses WHERE studentID=#{studentId}")
   List<StudentsCourses> searchStudentsCourses(Long studentID);
 
+  /**
+   * 受講生の新規登録を実行します。 受講生IDは1,2,3,4と自動的に設定されるようになっています。
+   * @param student 　受講生テーブル
+   */
   @Insert("INSERT INTO students(name,hurigana,nickname,address,area,years,gender,remark,isDeleted) "
       + "VALUES(#{name}, #{hurigana}, #{nickname}, #{address}, #{area}, #{years}, #{gender}, #{remark},false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
-
-  @Insert("INSERT INTO students_courses(studentID,courses,start,end)" +
-      "VALUES(#{studentID},#{courses},#{start},#{end})")
+  /**
+   * 受講生コース情報の新規登録を実行します。 受講生IDに紐づくようにstudentIDを設定しています。
+   * コースIDはA1,B1,C1と自動的に挿入されるようにMySQLで設定済みです。
+   * @param studentsCourses 　受講生コース情報
+   */
+  @Insert("INSERT INTO students_courses(studentId,courses,start,end)" +
+      "VALUES(#{studentId},#{courses},#{start},#{end})")
   void registerStudentsCourses(StudentsCourses studentsCourses);
 
-
+  /**
+   * 受講生IDによる受講生検索を実行します。
+   * @param id 　受講生ID
+   * @return 受講生情報
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student findById(Long id);
 
+  /**
+   * 受講生IDによる受講生コース情報の検索を実行します。
+   * @param id 　受講生ID
+   * @return　受講生情報
+   */
   @Select("SELECT * FROM students_courses WHERE studentID = #{id}")
   List<StudentsCourses> findCoursesByStudentId(Long id);
 
+  /**
+   * 受講生IDによる受講生検索を行い、該当受講生の更新処理を実行します。
+   * @param student 　受講生テーブル
+   */
   @Update("UPDATE students SET name = #{name}, hurigana = #{hurigana}, nickname = #{nickname}, "
       + "address = #{address}, area = #{area}, years = #{years}, gender = #{gender}, "
       + "remark = #{remark} WHERE id = #{id}")
   void updateStudent(Student student);
 
+  /**
+   * 受講生IDによるコース検索を実行し、該当コース情報の更新処理を実行します。
+   * @param studentsCourses 　受講生コース情報テーブル
+   */
   @Update("UPDATE students_courses SET courses = #{courses} "
       + " WHERE studentID = #{studentId}")
   void updateStudentsCourses(StudentsCourses studentsCourses);
--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -7,50 +7,54 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.repository.StudentRepository;
 
+/**
+ * 受講生情報を取り扱うサービスです。 検索・登録・更新を実行します。
+ */
 @Service
 public class StudentService {
 
   private StudentRepository repository;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentService(StudentRepository repository) {
+  public StudentService(StudentRepository repository, StudentConverter converter) {
     this.repository = repository;
+    this.converter = converter;
   }
 
-  public List<Student> searchStudentList() {
-
-    return repository.search();
+  /**
+   * 受講生一覧検索。 条件指定なしの全件検索を行います。
+   * @return　受講生一覧
+   */
+  public List<StudentDetail> searchStudentList() {
+    List<Student> studentList=repository.search();
+    List<StudentsCourses> studentsCoursesList=repository.searchStudentsCoursesList();
+    return converter.convertStudentDetails(studentList, studentsCoursesList);
   }
 
+  /**
+   * 受講生検索を実行します。 受講生IDに紐づく受講生情報を取得し、その受講生のコース情報を取得・設定します。
+   * @param id 　受講生ID
+   * @return　受講生情報
+   */
   public StudentDetail searchStudent(long id) {
     Student student = repository.searchStudent(id);
     List<StudentsCourses> studentsCourses = repository.searchStudentsCourses(student.getId());
-    StudentDetail studentDetail = new StudentDetail();
-    studentDetail.setStudent(student);
-    studentDetail.setStudentsCourses(studentsCourses);
-    return studentDetail;
+    return new StudentDetail(student, studentsCourses);
   }
 
-  public List<StudentsCourses> searchStudentsCoursesList() {
-    return repository.searchStudentsCoursesList();
-  }
-
-
-  public Student getStudentById(Long id) {
-    return repository.findById(id);
-  }
-
-  public List<StudentsCourses> getCoursesByStudentID(Long id) {
-    return repository.findCoursesByStudentId(id);
-  }
-
+  /**
+   * 受講生の新規登録を実行します。
+   * @param studentDetail　受講生詳細情報
+   */
   @Transactional
-  public void registerStudent(StudentDetail studentDetail) {
+  public StudentDetail registerStudent(StudentDetail studentDetail) {
     repository.registerStudent(studentDetail.getStudent());
     for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
       studentsCourses.setStudentId(studentDetail.getStudent().getId());
@@ -58,11 +62,17 @@ public class StudentService {
       studentsCourses.setEnd(String.valueOf(LocalDateTime.now().plusYears(1)));
       repository.registerStudentsCourses(studentsCourses);
     }
+    return studentDetail;
   }
 
+  /**
+   * 受講生の更新処理を実行します。
+   * System.out.println(~getCourses());はデバッグ用のコードです。
+   * @param studentDetail　受講生詳細情報
+   */
   @Transactional
   public void updateStudent(StudentDetail studentDetail) {
-    System.out.println("Updating student: " + studentDetail.getStudent().getId());
+
     repository.updateStudent(studentDetail.getStudent());
 
     for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
@@ -74,6 +84,3 @@ public class StudentService {
   }
 
 }
-
-
-
